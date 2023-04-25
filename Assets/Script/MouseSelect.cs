578fca8eab3afa367a08985100a9c59b32aef5d9
RK <riku.kuu0421@gmail.com>
2023-04-25 23:51:38 +0900

    [add]MouseSelectクラスを作成

---
+++ Assets/Script/MouseSelect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelect : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
